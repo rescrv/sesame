a373ce1c231516567f1c5fc8a618d1c31f56739c
Robert Escriva <robert@rescrv.net>
2018-05-20 22:11:26 -0700

    Support for FreeBSD

--- sesame.c
+++ sesame.c
@@ -31,6 +31,7 @@
 #include <assert.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 
 /* POSIX */
 #include <arpa/inet.h>
--- test/env.sh
+++ test/env.sh
@@ -1,6 +1,2 @@
-export SESAME_SRCDIR="$1"
-export SESAME_BUILDDIR="$2"
-export SESAME_VERSION="$3"
-
 export SESAME=$SESAME_BUILDDIR/test/sesame
 export UTILS=$SESAME_SRCDIR/test/util.sh
